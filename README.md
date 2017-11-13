816a3ae207afe2bf7ef18898500cdb413af1dae8
Eric Colinet <ecolinet@gmail.com>
2017-11-13 17:25:56 +0100

    Initial import

---
+++ README.md
@@ -0,0 +1,109 @@
+# Large File uploader
+
+The class is a way to make use of PHP's built-in [`enable_post_data_reading`](http://php.net/manual/en/ini.core.php#ini.enable-post-data-reading) setting.
+
+The idea is to allow files upload with a very low memory consumption.
+
+# How to ?
+
+
+## Deactivate `enable_post_data_reading`
+First be sure to set `enable_post_data_reading` to `0` on a _specific_ directory (e.g. `public/upload/`).
+
+There are [many ways to do that](http://php.net/manual/en/configuration.changes.php), 
+but keep in mind that, since this directive is `PHP_INI_PER_DIR`, it can't be changed
+with `ini_set` (it's logical since `_POST` & `_FILES` are processed _before_ the script runs). 
+
+And since it changes in many ways how PHP behaves it is not a good idea to set it globally (e.g. in `php.ini`).
+
+
+### .htaccess
+
+Here is an example with _apache2_ & _mod_php_ with `.htaccess` overriding enabled :
+
+```
+# Root of all things
+php_flag enable_post_data_reading 0
+```
+
+**Note :** of course it is a [better idea to do that in the main configuration](https://httpd.apache.org/docs/2.4/howto/htaccess.html#when).
+
+### .user.ini
+
+Here is an example if you are using `php-fpm` :
+
+```
+; Root of all things
+enable_post_data_reading=0
+```
+
+**Note :** unlike `.htaccess` files, that kind of files are cached among requests (300s per default), so you can safely use them.
+
+
+## Use the class
+
+Here is a small snippet of how to use it :
+
+```php
+$uploader = new \LargeFile\Uploader();
+$parts = $uploader->read();
+```
+
+`$parts` will be an array of elements composed of :
+
+- `headers` : array of all [`MIME`](https://en.wikipedia.org/wiki/MIME) headers sent by the browser
+- _whether_ :
+  - `file` : local filename of an uploaded file
+  - `content` : content of a posted field
+
+## Typical setup
+
+### Tree-ish view
+
+```
+my-app
+└── public
+    └── upload
+        ├── .htaccess
+        ├── .user.ini
+        └── index.php
+```
+
+### `index.php`
+
+```php
+<?php
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+try {
+
+    // Get uploaded files
+    $uploader = new \LargeFile\Uploader();
+    $parts = $uploader->read();
+
+} catch (\Exception $e) {
+
+    header('Content-type: application/json; charset=utf-8', true, 500);
+    echo json_encode([
+        'status'  => "KO",
+        'message' => "Error !",
+    ]);
+    error_log(get_class($e) . ': ' . $e->getMessage());
+}
+
+// Persist files out of tmp
+foreach ($parts as $part) {
+    $filename = $part['headers']['filename'];
+    rename(
+        $part['file'],
+        __DIR__ . '/../../data/upload/' . uniqid() . '-' . $filename
+    );
+}
+
+// Send success status
+header('Content-type: application/json; charset=utf-8', true, 200);
+echo json_encode([
+    'status'  => "OK",
+    'message' => "File(s) uploaded",
+]);
+```
---
+++ src/Uploader.php
@@ -0,0 +1,242 @@
+<?php
+
+namespace LargeFile;
+
+class Uploader
+{
+    /**
+     * $_SERVER to use
+     *
+     * @var array[string]
+     */
+    protected $_server;
+
+    /**
+     * Read buffer
+     *
+     * @var string
+     */
+    protected $buffer;
+
+    /**
+     * Handle of the input file
+     *
+     * @var resource
+     */
+    protected $inputHandle;
+
+    /**
+     * MIME boundary
+     *
+     * @var string
+     */
+    protected $boundary;
+
+    /**
+     * Temporary directory
+     *
+     * @var string
+     */
+    protected $tmpDir;
+
+    /**
+     * Size of the read buffer
+     *
+     * @see fread()
+     * @var int
+     */
+    protected $bufferSize;
+
+    /**
+     * Uploader constructor.
+     *
+     * Optionally provide à $_SERVER array
+     *
+     * @param array $_server
+     */
+    public function __construct(array $_server = null, $tmpDir = null, $bufferSize = null)
+    {
+        $this->_server = $_server ?: $_SERVER;
+        $this->tmpDir = $tmpDir ?: sys_get_temp_dir();
+        $this->bufferSize = $bufferSize ?: 8192;
+
+        if (ini_get('enable_post_data_reading')) {
+            $this->handleError("'enable_post_data_reading' setting must be off");
+        }
+    }
+
+    /**
+     * Read input file for MIME parts
+     *
+     * @param string $inputFile
+     * @return array
+     */
+    public function read($inputFile = 'php://input')
+    {
+        try {
+            if ($this->_server['REQUEST_METHOD'] !== 'POST') {
+                $this->handleError();
+            }
+
+            //$totalLength = $this->_server['CONTENT_LENGTH'];
+            $contentTypeParts = preg_split('/; boundary=/', $this->_server['CONTENT_TYPE']);
+
+            if ($contentTypeParts[0] !== 'multipart/form-data') {
+                $this->handleError();
+            }
+            if (!isset($contentTypeParts[1]) || empty($contentTypeParts[1])) {
+                $this->handleError();
+            }
+            $this->boundary = '--' . $contentTypeParts[1];
+
+            // Open file handle
+            $this->inputHandle = fopen($inputFile, 'rb');
+            if (!$this->inputHandle) {
+                $this->handleError();
+            }
+
+            $parts = array();
+            $this->buffer = '';
+            do {
+                $part = $this->readMimePart();
+                if ($part === null) {
+                    break;
+                }
+                $parts[] = $part;
+            } while (1);
+
+        } catch (\Exception $e) {
+            $this->handleError($e);
+        }
+
+        return $parts;
+    }
+
+    protected function readMimePart()
+    {
+        if (strlen($this->buffer) < strlen($this->boundary) + 2) {
+            $this->buffer .= rtrim(fgets($this->inputHandle));
+        }
+
+        // Part
+        $part = array();
+
+        // Check end
+        if (strpos($this->buffer, $this->boundary . '--') === 0) { // End
+            return null;
+        }
+
+        // Check boundary
+        if (strpos($this->buffer, $this->boundary) !== 0) { // read boundary
+            $this->handleError($this->buffer, $this->boundary);
+        }
+
+        // Get buffer w/o boundary
+        $this->buffer = substr($this->buffer, strlen($this->boundary));
+
+        // Read headers
+        $headers = array();
+        while (!feof($this->inputHandle)) {
+            $header = rtrim(fgets($this->inputHandle));
+
+            if ($header == '') { // Skip headers if the line is empty
+                break;
+            }
+
+            $headerParts = preg_split('/; /', $header);
+            if (!is_array($headerParts) || !count($headerParts)) {
+                $this->handleError();
+            }
+
+            // First header line
+            $mainHeader = preg_split('/: /', $headerParts[0]);
+            if (!is_array($mainHeader) || count($mainHeader) != 2) {
+                $this->handleError();
+            }
+            $headers[$mainHeader[0]] = $mainHeader[1];
+
+            // Other header lines
+            for ($i = 1; $i < count($headerParts); $i++) {
+                $headerLineParts = preg_split('/=/', $headerParts[$i]);
+                if (!is_array($headerLineParts) || count($headerLineParts) != 2) {
+                    $this->handleError();
+                }
+                $headers[$headerLineParts[0]] = $headerLineParts[1];
+            }
+        }
+
+        // Check header
+        if (!isset($headers['name'])) {
+            $this->handleError();
+        }
+        $headers['name'] = trim($headers['name'], '"');
+
+        $isFile = false;
+        if (isset($headers['filename'])) {
+            $isFile = true;
+            $headers['filename'] = basename(trim($headers['filename'], '"'));
+        }
+
+        $part['headers'] = $headers;
+
+        $copyEnd = false;
+        $outputFile = null;
+        $outFileName = null;
+
+        if ($isFile) {
+            $outFileName = tempnam($this->tmpDir, 'lfu');
+            $outputFile = fopen($outFileName, 'wb');
+            $part['file'] = $outFileName;
+        } else {
+            $part['content'] = '';
+        }
+
+        // Read content
+        while (!$copyEnd && !feof($this->inputHandle)) {
+            $this->buffer .= fread($this->inputHandle, $this->bufferSize);
+
+            // Check if boundary can be found
+            $boundaryPos = strpos($this->buffer, $this->boundary);
+            if ($boundaryPos !== false) {
+                $strWrite = substr($this->buffer, 0, $boundaryPos - 2);
+                $this->buffer = substr($this->buffer, $boundaryPos);
+                $copyEnd = true;
+            } else if (strlen($this->buffer) >= $this->bufferSize - strlen($this->boundary)) {
+                // Copy until an eventual '-' (to avoid boundary truncation)
+                $minusPos = strpos($this->buffer, '-', $this->bufferSize - strlen($this->boundary) + 2);
+                if ($minusPos !== false) {
+                    $strWrite = substr($this->buffer, 0, $minusPos);
+                    $this->buffer = substr($this->buffer, $minusPos);
+                } else {
+                    $strWrite = $this->buffer;
+                    $this->buffer = '';
+                }
+            } else {
+                $strWrite = $this->buffer;
+                $this->buffer = '';
+            }
+
+            if ($isFile) {
+                fwrite($outputFile, $strWrite);
+            } else {
+                $part['content'] .= $strWrite;
+            }
+        }
+
+        if ($isFile) {
+            fclose($outputFile);
+        }
+
+        return $part;
+    }
+
+
+    protected function handleError($error = false)
+    {
+        if ($error instanceof \Exception) {
+            throw $error;
+        }
+
+        throw new \Exception($error ?: 'Unable get MIME data');
+    }
+}
